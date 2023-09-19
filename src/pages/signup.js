4b45b22d48373e072c11528aa2e35480ab9b3614
OleanjiKingCode <holamilean56@gmail.com>
2023-09-19 16:24:18 +0100

    started form validation

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,10 +2,20 @@ import { useMultipleForm } from "@/context/useMultipleForm";
 import React from "react";
 import { fondamento } from "./_app";
 import Step1 from "./step1";
+import { useForm, SubmitHandler } from "react-hook-form";
 import Step2 from "./step2";
 
 export default function Signup() {
-  const arraySteps = [<Step1 key="step1" />, <Step2 key="step2" />];
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const arraySteps = [
+    <Step1 key="step1" register={register} errors={errors} />,
+    <Step2 key="step2" />,
+  ];
   const {
     currentStepIndex,
     steps,
@@ -15,31 +25,40 @@ export default function Signup() {
     isFirstIndex,
     isLastIndex,
   } = useMultipleForm(arraySteps);
+
+  const onSubmit = ({ firstName }) => {
+    console.log(firstName);
+    next();
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="bg-white py-5 w-[40%] h-[50%] px-9 rounded-lg shadow-md">
-        {step}
-        <div
-          className={`w-full flex flex-row ${
-            isFirstIndex ? "justify-end" : "justify-between"
-          } items-center`}
-        >
-          {!isFirstIndex && (
-            <button
-              className=" rounded-lg text-md py-2 px-5 text-center bg-black text-white "
-              onClick={previous}
-            >
-              Back
-            </button>
-          )}
-
-          <button
-            className=" rounded-lg text-md py-2 px-5 text-center bg-black text-white "
-            onClick={next}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {step}
+          <div
+            className={`w-full flex flex-row ${
+              isFirstIndex ? "justify-end" : "justify-between"
+            } items-center`}
           >
-            {isLastIndex ? "Finish" : "Next"}
-          </button>
-        </div>
+            {!isFirstIndex && (
+              <button
+                className=" rounded-lg text-md py-2 px-5 text-center bg-black text-white "
+                onClick={previous}
+                type="button"
+              >
+                Back
+              </button>
+            )}
+
+            <button
+              className=" rounded-lg text-md py-2 px-5 text-center bg-black text-white"
+              type="submit"
+            >
+              {isLastIndex ? "Finish" : "Next"}
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
--- src/pages/step1.js
+++ src/pages/step1.js
@@ -2,8 +2,9 @@ import React from "react";
 import { fondamento } from "./_app";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Link from "next/link";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-const Step1 = () => {
+const Step1 = ({ register, errors }) => {
   return (
     <div>
       <div
@@ -12,28 +13,66 @@ const Step1 = () => {
         Sign Up
       </div>
       <div className="py-6 flex flex-col space-y-8">
-        <div className="flex flex-row w-full space-x-3 items-center">
-          <span className="w-[60%]">Official Name:</span>{" "}
-          <input
-            type="text"
-            className="w-full h-6 py-4 px-3 border-slate-600 border-[1px] rounded-lg"
-          />
+        <div className="flex flex-col w-full space-y-3 ">
+          <div className="flex flex-row w-full space-x-3 items-center">
+            <label className="w-[60%]">Official Name:</label>{" "}
+            <input
+              type="text"
+              className="w-full h-6 py-4 px-3 border-slate-600 border-[1px] rounded-lg"
+              id="name"
+              placeholder="Enter your Official Name"
+              {...register("name", {
+                required: "Please enter your Official Name",
+                minLength: {
+                  value: 6,
+                  message: "Official Name should be more than 6 chars",
+                },
+              })}
+            />
+          </div>
+          {errors.name && (
+            <div className="py-1 text-red-500">{errors.name.message}</div>
+          )}
         </div>
-        <div className="flex flex-row space-x-3 items-center ">
-          <span className="w-[60%]">Nickname:</span>{" "}
-          <input
-            type="text"
-            className="w-full h-6 py-4 px-3 border-slate-600 border-[1px] rounded-lg"
-          />
+        <div className="flex flex-col w-full space-y-3 ">
+          <div className="flex flex-row space-x-3 items-center ">
+            <label className="w-[60%]">Nickname:</label>{" "}
+            <input
+              type="text"
+              className="w-full h-6 py-4 px-3 border-slate-600 border-[1px] rounded-lg"
+              id="nick"
+              placeholder="Enter your Official Name"
+              {...register("nick", {
+                required: "Please enter your nickname or common name you go by",
+              })}
+            />
+          </div>
+          {errors.nick && (
+            <div className="py-1 text-red-500">{errors.nick.message}</div>
+          )}
         </div>
-        <div className="flex flex-row space-x-3 items-center">
-          <span className="w-[60%]">Email:</span>{" "}
-          <input
-            type="text"
-            className="w-full h-6 py-4 px-3 border-slate-600 border-[1px] rounded-lg"
-          />
+        <div className="flex flex-col w-full space-y-3 ">
+          <div className="flex flex-row space-x-3 items-center">
+            <label className="w-[60%]">Email:</label>{" "}
+            <input
+              className="w-full h-6 py-4 px-3 border-slate-600 border-[1px] rounded-lg"
+              id="email"
+              type="email"
+              placeholder="Enter your email address"
+              {...register("email", {
+                required: "Please enter email",
+                pattern: {
+                  value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                  message: "Please enter valid email",
+                },
+              })}
+            />
+          </div>
+          {errors.email && (
+            <div className="text-red-500 py-1">{errors.email.message}</div>
+          )}
         </div>
-        <div className="flex  justify-center">
+        <div className="flex justify-center">
           <ConnectButton />
         </div>
 
