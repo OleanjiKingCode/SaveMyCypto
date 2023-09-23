7dabe79544feee9d64b8283af3540b12f820438c
OleanjiKingCode <holamilean56@gmail.com>
2023-09-23 08:42:18 +0100

    profile page UI

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -12,6 +12,25 @@ const Id = () => {
   let categories = ["About", "Savings", "Plans"];
   const { isConnected, address } = useAccount();
 
+  const Details = (cat) => {
+    if (cat === "About") {
+      return <div className="">GM, Oleanji</div>;
+    } else if (cat === "Savings") {
+      return (
+        <div className="grid grid-cols-4 gap-8  w-full">
+          {[1, 2, 3, 4, 5, 6, 7, 8].map((num, i) => (
+            <div
+              className="bg-slate-200 w-[50%] ml-[24%] rounded-2xl h-20 text-black flex flex-row items-center justify-center"
+              key={i}
+            >
+              <span>{num}</span>
+            </div>
+          ))}
+        </div>
+      );
+    }
+  };
+
   return (
     <div>
       <div className="relative">
@@ -27,13 +46,13 @@ const Id = () => {
       </div>
       <div className="w-full px-2 pt-4 pb-10 sm:px-0">
         <Tab.Group>
-          <Tab.List className="flex max-w-3xl mr-3 space-x-1 rounded-xl bg-blue-900/20 p-1 ml-auto">
+          <Tab.List className="flex max-w-lg mr-3 space-x-1 rounded-xl bg-blue-900/20 p-1 ml-auto">
             {categories.map((category, i) => (
               <Tab
                 key={i}
                 className={({ selected }) =>
                   classNames(
-                    "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
+                    "w-full rounded-lg py-2 text-sm font-medium leading-5 text-blue-700",
                     "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
                     selected
                       ? "bg-white shadow"
@@ -50,11 +69,13 @@ const Id = () => {
               <Tab.Panel
                 key={idx}
                 className={classNames(
-                  "bg-white py-3 px-5  flex justify-center ",
+                  "bg-white pt-3 pb-1 px-5  flex justify-center ",
                   "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
                 )}
               >
-                <div className="font-semibold text-2xl">{item}</div>
+                <div className="font-semibold text-2xl w-full">
+                  {Details(item)}
+                </div>
               </Tab.Panel>
             ))}
           </Tab.Panels>
